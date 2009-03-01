8f122b30a7281fd2b9b559ee982c4afe303ece59
Naitik Shah <naitik.shah@yahoo.com>
2009-03-01 14:08:39 -0800

    renamed projects, made projects.py execution location agnostic

--- projects/projects.py
+++ projects/projects.py
@@ -4,7 +4,10 @@ import json
 import os
 from jinja2 import Template
 
+__dir__ = os.path.realpath(os.path.dirname(__file__))
+def f(n):
+    return file(os.path.join(__dir__, n))
 
-template = Template(file('projects.html').read())
-projects = json.load(file('projects.json'))
+template = Template(f('projects.html').read())
+projects = json.load(f('projects.json'))
 print template.render(sections=projects)
